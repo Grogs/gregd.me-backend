d594c959135da7d983e3d1cab3220445c177b8b5
Greg Dorrell <Greg@memimo.co.uk>
2012-03-12 16:45:26 +0000

    about the test JPA stuff

--- src/main/scala/me/gregd/www/data/TweetDAO.scala
+++ src/main/scala/me/gregd/www/data/TweetDAO.scala
@@ -1,9 +1,16 @@
 package me.gregd.www.data
 import javax.persistence._
+import me.gregd.www.model.Tweet
 
 object TweetDAO {
 
   @PersistenceContext
   var em:EntityManager = _
 
+  
+  def getTweets() {
+    val queryString = "SELECT * FROM tweets"
+    em.createQuery(queryString).getResultList().asInstanceOf[java.util.List[Tweet]]
+  }
+  
 }
