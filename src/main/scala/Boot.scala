29d5004504a8089e7343969f55d970db90bec3e5
Greg Dorrell <greg@dorrell.me>
2013-03-13 21:11:17 +0000

    Disable spray-can stuff, should never have made it onto this branch.

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -6,15 +6,15 @@ import spray.can.server.HttpServer
 import spray.io.SingletonHandler
 
 object Boot extends App {
-  val system = ActorSystem("gregd")
-  
-  val ioBridge = new IOBridge(system).start()
-    
-  val service = system.actorOf(Props[AllServicesActor])
-  
-  val httpServer = system.actorOf(Props(new HttpServer(ioBridge, SingletonHandler(service))))
-
-  httpServer ! HttpServer.Bind("localhost", 8080)
-  
-  system.registerOnTermination(ioBridge.stop)
+//  val system = ActorSystem("gregd")
+//
+//  val ioBridge = new IOBridge(system).start()
+//
+//  val service = system.actorOf(Props[AllServicesActor])
+//
+//  val httpServer = system.actorOf(Props(new HttpServer(ioBridge, SingletonHandler(service))))
+//
+//  httpServer ! HttpServer.Bind("localhost", 8080)
+//
+//  system.registerOnTermination(ioBridge.stop)
 }
